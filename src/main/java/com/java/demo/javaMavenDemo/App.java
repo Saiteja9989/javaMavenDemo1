d82772dfec1085257f91e2452a8181a17d36fd1b
saiteja756 <kasojusaiteja1001@gmail.com>
2024-11-14 16:03:39 +0530

    files commited

---
+++ src/main/java/com/java/demo/javaMavenDemo/App.java
@@ -0,0 +1,13 @@
+package com.java.demo.javaMavenDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "SAITEJA 22BD1A054V" );
+    }
+}
